c2ee3f74c418ac2de13eb79a320f8063a2cd4373
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-13 14:26:56 +0100

    json

--- code.js
+++ code.js
@@ -1,40 +1,40 @@
 function renderMovie(movie){
-    document.getElementsByClassName("Title")[0].innerText = movie.Title;
-    document.getElementsByClassName("Desc")[0].innerText = movie.Desc;
-
-
-    let actorList ="";
+    $(".Title").text(movie.Title);
+    $(".Desc").text(movie.Desc);
+    
+    $(".Actors").empty();
     for (let index = 0; index < movie.Actors.length; index++) {
-
-        actorList += "<li>" + movie.Actors[index] + "</li>";
+        
+        $(".Actors").append("<li>" + movie.Actors[index] + "</li>");
         
     }
-    document.getElementsByClassName("Actors")[0].innerHTML = actorList;
-        
-    document.getElementsByClassName("Poster")[0].setAttribute("src", movie.Image);
+    $(".Poster").attr("src", movie.Image);        
+
 }
 
 
 function changeStarRating(grade){
-    for (let i = 1; i <= 5; i++) {
-        let star = document.getElementById("star" + i);
-        if (i <= grade) {
-            star.classList.add("filled");
+ 
+        $(".filled").removeClass("filled");
+        for (let i = 1; i <= grade; i++) {
+            $("[data-rating-id=" + i + "]").addClass("filled");
+            
         }
-        else
-        {
-            star.classList.remove("filled")
-        }
-    }
-}
-for (let i = 1; i <= 5; i++) {
-    let star = document.getElementById("star" + i);
-    star.addEventListener("click", () =>{
-        changeStarRating(i);
-    });
     
 }
+$(".starRating").on("click", "span", function(e){
+
+    let star = $(e.target);
+  
+    let rating = parseInt(star.attr("data-rating-id"));
+  
+    changeStarRating(rating);
+  
+  });
+
+ 
+
 
 
 renderMovie(movieData);
-changeStarRating(2);
+//changeStarRating(2);

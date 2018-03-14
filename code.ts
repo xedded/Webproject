34e23b10fda4ddbb7daf951631fbad8dba0f12f7
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-14 15:41:06 +0100

    AngularJS convert...... And shit!

--- code.ts
+++ code.ts
@@ -1,36 +1,10 @@
-import * as $ from "jquery";
 import {movieData} from "./data";
-import { Review } from "./interfaces";
-// let movieData = require("./data");
-
-function renderMovie(data: Review) {
-    $(".Title").text(data.Title);
-    $(".Desc").text(data.Desc);
-    $(".Actors").empty();
-    for  (let index = 0; index < data.Actors.length; index++) {
-
-        $(".Actors").append("<li>" + data.Actors[index] + "</li>");
-
-    }
-    $(".Poster").attr("src", data.Image);
-}
-
-function changeStarRating(grade) {
-    $(".filled").removeClass("filled");
-    for (let i = 1; i <= grade; i++) {
-        $("[data-rating-id=" + i + "]").addClass("filled");
-
-    }
-}
-$(".starRating").on("click", "span", (e) => {
-
-    const star = $(e.target);
-
-    const rating = parseInt(star.attr("data-rating-id"));
-
-    changeStarRating(rating);
-
+import * as angular from "angular";
+let app = angular.module("reviewApp", []);
+app.controller("reviewController", function($scope){
+    $scope.movie = movieData;
+    $scope.rating = 0;
+    $scope.changeStarRating = function(newRating){
+        $scope.rating = newRating;
+      }
 });
-
-renderMovie(movieData);
-// changeStarRating(2);

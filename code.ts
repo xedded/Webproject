6ceeef7a53629bdeea68703bcf9a62f40040aaf5
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-14 16:41:19 +0100

    Orkar inte ens.. (shit)

--- code.ts
+++ code.ts
@@ -1,10 +1,39 @@
-import {movieData} from "./data";
+import {movieData1, movieData2} from "./data";
 import * as angular from "angular";
+import {Review} from "./interfaces";
 let app = angular.module("reviewApp", []);
-app.controller("reviewController", function($scope){
-    $scope.movie = movieData;
-    $scope.rating = 0;
-    $scope.changeStarRating = function(newRating){
-        $scope.rating = newRating;
+app.controller("appController", function(){
+    this.movie1 = movieData1;
+    this.movie2 = movieData2;
+  });
+  
+  app.component("review", {
+    bindings: {
+      movie: '<'
+    },
+  
+    controller: class ReviewComponent {
+      rating = 0;
+      movie: Review;
+      changeStarRating(newRating){
+        this.rating = newRating;
       }
+    },
+  
+    controllerAs: 'reviewCtrl',
+    template: `
+    <div class="container">
+    <div><img class="poster imgPoster" ng-src="{{reviewCtrl.movie.Image}}"></div>
+    <div class="desc">
+        <div class="starRating">
+            <span ng-repeat="i in [1,2,3,4,5,6,7,8,9,10]" ng-class="{filled: reviewCtrl.rating >= i}" ng-click="reviewCtrl.changeStarRating(i)">&#9733;</span>
+        </div>
+        <h3 class="Title">{{reviewCtrl.movie.Title}}</h3>
+        <p class="Desc">{{reviewCtrl.movie.Desc}}</p>
+        <ul class="ul1 Actors">
+            <li ng-repeat="item in reviewCtrl.movie.Actors">{{item}}</li>
+        </ul>
+    </div>
+</div>
+    `
 });
--- data.ts
+++ data.ts
@@ -1,8 +1,16 @@
 import {Review} from "./interfaces";
-export const movieData: Review = {
-    Title: "INCEPTION",
+export const movieData1: Review = {
+    Title: "Inception",
     Desc: "Dom Cobb is a skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets",
     Actors: ["Leonardo DiCaprio", "Ellen Page", "Tom Hardy"],
     Image: "http://cdn.collider.com/wp-content/uploads/Inception-movie-poster-2.jpg"
+
+  };
+
+  export const movieData2: Review = {
+    Title: "The Shawshank Redemption",
+    Desc: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    Actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+    Image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
 
   };

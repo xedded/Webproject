9a31d2d4bbc3ae5a7a10c0e456ca7ff832ea8231
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-14 09:43:34 +0100

    Awesome changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 let path = require("path");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
     mode: 'development',
     entry: './code.js',
     output: {
-      path: path.resolve(__dirname, "."),
+      path: path.resolve(__dirname, "distribution"),
       filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "distribution"), // the root for the server
+        watchContentBase: true, // so we reload if other stuff like CSS changes
+        port: 9000, // it'll now be at http://localhost:9000
+    },
+    plugins: [
+    new WebpackNotifierPlugin({alwaysNotify: true}),
+    ]
+    
   };
